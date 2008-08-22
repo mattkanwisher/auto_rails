47e38ed47ec66e66b9cbeea4afd909c557421abd
Matthew Campbell <hyper@hyperworks.nu>
2008-08-22 11:34:44 -0400

    done parsing msn

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define do
     t.string    :city
     t.string    :state
     t.string    :county
+    t.float    :avg_price
   end
 
   create_table :gas_zips do |t|
@@ -30,6 +31,7 @@ ActiveRecord::Schema.define do
     t.string    :lat
     t.string    :in_page_id
     t.string    :gas_mini_name
-    t.string    :address
+    t.string    :address1
+    t.string    :address2    
   end
 end
