b2acc2eba5ae6a41e3cc942b6ca730e564d984ca
Matthew Campbell <mcampbell@hyperworks.nu>
2008-09-10 21:42:51 -0400

    test

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,6 +1,6 @@
 class ZipCode < ActiveRecord::Base
   def self.find_near_lat_long(latitude, longitude)
-      distance = 10
-      find_by_sql "SELECT * FROM zip_codes WHERE (3958*3.1415926*sqrt( (lat-#{latitude})*(lat-#{latitude})  + cos(lat/57.29578)*cos(#{latitude}/57.29578)*(longitude-#{longitude})*(longitude-#{longitude}) )/180  ) <= #{distance}"
+      distance = 20
+      find_by_sql "SELECT * FROM zip_codes WHERE (3958*3.1415926*sqrt( (lat-#{latitude})*(lat-#{latitude})  + cos(lat/57.29578)*cos(#{latitude}/57.29578)*(longitude-#{longitude})*(longitude-#{longitude}) )/180  ) <= #{distance} AND avg_price is NOT NULL"
   end
 end
